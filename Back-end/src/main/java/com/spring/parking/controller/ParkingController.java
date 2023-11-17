1c63fa42620a4176479cc6b72fe57f1c0db73b20
Badr Ouaddah <Badr.Ouaddah.07@gmail.com>
2023-11-17 01:00:11 +0100

    Create ParkingService class for Service layer and add getParking method

--- Back-end/src/main/java/com/spring/parking/controller/ParkingController.java
+++ Back-end/src/main/java/com/spring/parking/controller/ParkingController.java
@@ -3,12 +3,12 @@ package com.spring.parking.controller;
 
 import com.spring.parking.bean.Parking;
 import com.spring.parking.dao.ParkingDao;
+import com.spring.parking.service.ParkingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -18,8 +18,11 @@ public class ParkingController {
     @Autowired
     private ParkingDao parkingDao;
 
+    @Autowired
+    private ParkingService parkingService;
+
     @GetMapping("/getParking")
-    public Parking getParking(){
-        return parkingDao.findById(1L).orElse(null);
+    public List<Parking> getParking(){
+        return parkingService.getParking();
     }
 }
--- Back-end/src/main/java/com/spring/parking/service/ParkingService.java
+++ Back-end/src/main/java/com/spring/parking/service/ParkingService.java
@@ -1,4 +1,22 @@
 package com.spring.parking.service;
 
+import com.spring.parking.bean.Parking;
+import com.spring.parking.dao.ParkingDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ParkingService {
+    private ParkingDao parkingDao;
+
+    @Autowired
+    public ParkingService(ParkingDao parkingDao) {
+        this.parkingDao = parkingDao;
+    }
+
+    public List<Parking> getParking(){
+        return parkingDao.findAll();
+    }
 }
