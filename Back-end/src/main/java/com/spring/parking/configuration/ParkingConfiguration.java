dd790c14636bfa492aab79d8b78eb07fa87bc342
Badr Ouaddah <Badr.Ouaddah.07@gmail.com>
2023-11-17 01:17:19 +0100

    Add condition to create table one time only if table is empty

--- Back-end/src/main/java/com/spring/parking/configuration/ParkingConfiguration.java
+++ Back-end/src/main/java/com/spring/parking/configuration/ParkingConfiguration.java
@@ -14,14 +14,19 @@ public class ParkingConfiguration {
     @Bean
     CommandLineRunner commandLineRunner(ParkingDao parkingDao){
         return args -> {
-            Parking parking = new Parking();
-            parking.setPricePerplacePerminute(2);
-            parking.setOpenTime("7:00");
-            parking.setClosetime("12:00");
-            List<String> newParkingLot = Arrays.asList("Lot 1", "Lot 2", "Lot 3");
-            String parkingLotCSV = String.join(",", newParkingLot);
-            parking.setParkingLot(parkingLotCSV);
-            parkingDao.save(parking);
+            if (!parkingDataExists(parkingDao)) {
+                Parking parking = new Parking();
+                parking.setPricePerplacePerminute(2);
+                parking.setOpenTime("7:00");
+                parking.setClosetime("12:00");
+                List<String> newParkingLot = Arrays.asList("Lot 1", "Lot 2", "Lot 3");
+                String parkingLotCSV = String.join(",", newParkingLot);
+                parking.setParkingLot(parkingLotCSV);
+                parkingDao.save(parking);
+            }
         };
+    }
+    private boolean parkingDataExists(ParkingDao parkingDao){
+        return parkingDao.count() > 0;
     }
 }
