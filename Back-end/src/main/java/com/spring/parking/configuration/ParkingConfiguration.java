f355b9b9b953bcf0b3345f65d2fabccb47e3aa50
Badr Ouaddah <Badr.Ouaddah.07@gmail.com>
2023-11-19 19:27:59 +0100

    Configure to insert data in ParkingLot table

--- Back-end/src/main/java/com/spring/parking/configuration/ParkingConfiguration.java
+++ Back-end/src/main/java/com/spring/parking/configuration/ParkingConfiguration.java
@@ -1,32 +1,42 @@
 package com.spring.parking.configuration;
 
 import com.spring.parking.bean.Parking;
+import com.spring.parking.bean.ParkingLot;
 import com.spring.parking.dao.ParkingDao;
+import com.spring.parking.dao.ParkingLotDao;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Configuration
 public class ParkingConfiguration {
-//    @Bean
-//    CommandLineRunner commandLineRunner(ParkingDao parkingDao){
-//        return args -> {
-//            if (!parkingDataExists(parkingDao)) {
-//                Parking parking = new Parking();
-//                parking.setPricePerplacePerminute(2);
-//                parking.setOpenTime("7:00");
-//                parking.setClosetime("12:00");
-//                List<String> newParkingLot = Arrays.asList("Lot 1", "Lot 2", "Lot 3", "Lot 4", "Lot 5");
-//                String parkingLotCSV = String.join(",", newParkingLot);
-//                parking.setParkingLot(parkingLotCSV);
-//                parkingDao.save(parking);
-//            }
-//        };
-//    }
-//    private boolean parkingDataExists(ParkingDao parkingDao){
-//        return parkingDao.count() > 0;
-//    }
-}
+    @Bean
+    CommandLineRunner commandLineRunner(ParkingDao parkingDao, ParkingLotDao parkingLotDao){
+        return args -> {
+            Parking parking = new Parking();
+            ParkingLot parkingLot = new ParkingLot();
+
+            if (!parkingDataExists(parkingDao)) {
+                parking.setPricePerplacePerminute(2);
+                parking.setOpenTime("7:00");
+                parking.setClosetime("12:00");
+
+                parkingDao.save(parking);
+            }
+            parkingLot.setParkingLotNumber(1);
+            parkingLot.setCarParkingInfo("First Car");
+            parkingLotDao.save(parkingLot);
+
+        };
+    }
+
+
+    private boolean parkingDataExists(ParkingDao parkingDao){
+        return parkingDao.count() > 0;
+    }
+
+    }
