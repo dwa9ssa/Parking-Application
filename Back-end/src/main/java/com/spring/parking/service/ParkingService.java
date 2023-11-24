b0424411cfe60d7480afe7f1e331f49ad82f4c14
Badr Ouaddah <Badr.Ouaddah.07@gmail.com>
2023-11-24 15:30:59 +0100

    Add method to create Parking with 30 parking lots

--- Back-end/src/main/java/com/spring/parking/service/ParkingService.java
+++ Back-end/src/main/java/com/spring/parking/service/ParkingService.java
@@ -1,15 +1,18 @@
 package com.spring.parking.service;
 
 import com.spring.parking.bean.Parking;
+import com.spring.parking.bean.ParkingLot;
 import com.spring.parking.dao.ParkingDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class ParkingService {
     private ParkingDao parkingDao;
+    private Parking parking;
 
     @Autowired
     public ParkingService(ParkingDao parkingDao) {
@@ -21,6 +24,13 @@ public class ParkingService {
     }
 
     public Parking parkingInit(Parking parking){
+        List<ParkingLot> parkingLots = new ArrayList<>();
+        for (int i = 1; i <= 30; i++) {
+            ParkingLot parkingLot = new ParkingLot();
+            parkingLot.setParkingLotNumber(i);
+            parkingLots.add(parkingLot);
+        }
+        parking.setParkingLot(parkingLots);
         return parkingDao.save(parking);
     }
 
