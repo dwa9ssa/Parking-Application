95bfd6b4145a3369bc0aca66fa7b59ebd2729d7a
Badr Ouaddah <Badr.Ouaddah.07@gmail.com>
2023-11-29 18:17:03 +0100

    Add ParkingConfiguration class to configure parking table while the app is running.

--- Back-end/src/main/java/com/spring/parking/configuration/ParkingConfiguration.java
+++ Back-end/src/main/java/com/spring/parking/configuration/ParkingConfiguration.java
@@ -1,38 +1,31 @@
-//package com.spring.parking.configuration;
-//
-//import com.spring.parking.bean.Parking;
-//import com.spring.parking.bean.ParkingLot;
-//import com.spring.parking.dao.ParkingDao;
-//import com.spring.parking.dao.ParkingLotDao;
-//import org.springframework.boot.CommandLineRunner;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//import java.util.ArrayList;
-//import java.util.Arrays;
-//import java.util.List;
-//
-//@Configuration
-//public class ParkingConfiguration {
-//    @Bean
-//    CommandLineRunner commandLineRunner(ParkingDao parkingDao, ParkingLotDao parkingLotDao){
-//        return args -> {
-//            Parking parking = new Parking();
-//            ParkingLot parkingLot = new ParkingLot();
-//
-//            if (!parkingDataExists(parkingDao)) {
-//                parking.setPricePerplacePerminute(2);
-//                parking.setOpenTime("7:00");
-//                parking.setClosetime("12:00");
-//
-//                parkingDao.save(parking);
-//            }
-//        };
-//    }
-//
-//
-//    private boolean parkingDataExists(ParkingDao parkingDao){
-//        return parkingDao.count() > 0;
-//    }
-//
-//    }
+package com.spring.parking.configuration;
+
+import com.spring.parking.entity.Parking;
+import com.spring.parking.dao.ParkingDao;
+import com.spring.parking.dao.ParkingLotDao;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class ParkingConfiguration {
+    @Bean
+    CommandLineRunner commandLineRunner(ParkingDao parkingDao, ParkingLotDao parkingLotDao){
+        return args -> {
+            Parking parking = new Parking();
+            if (!parkingDataExists(parkingDao)) {
+                parking.setOpenTime("7:00 AM");
+                parking.setClosetime("12:00 PM");
+
+                parkingDao.save(parking);
+            }
+        };
+    }
+
+
+    private boolean parkingDataExists(ParkingDao parkingDao){
+        return parkingDao.count() > 0;
+    }
+
+    }
