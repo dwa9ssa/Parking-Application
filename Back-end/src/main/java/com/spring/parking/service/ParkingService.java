7856f60393064a4488c1b4d2f718f09095fb1472
Badr Ouaddah <Badr.Ouaddah.07@gmail.com>
2023-12-04 12:04:24 +0100

    Add Transactional annotation in service layer

--- Back-end/src/main/java/com/spring/parking/service/ParkingService.java
+++ Back-end/src/main/java/com/spring/parking/service/ParkingService.java
@@ -6,10 +6,12 @@ import com.spring.parking.dao.ParkingDao;
 import com.spring.parking.entity.ParkingLot;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional
 public class ParkingService {
     private ParkingDao parkingDao;
     @Autowired
