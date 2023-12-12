2c8f9f2a583dd47b3f5fa1855298ae786c2d5a26
Badr Ouaddah <Badr.Ouaddah.07@gmail.com>
2023-12-12 18:28:14 +0100

    Create mock object and add Assertions in contextLoads test method

--- Back-end/src/test/java/com/spring/parking/ParkingApplicationTests.java
+++ Back-end/src/test/java/com/spring/parking/ParkingApplicationTests.java
@@ -1,13 +1,20 @@
 package com.spring.parking;
 
+import com.spring.parking.entity.CarParkingInfo;
+import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ParkingApplicationTests {
+	@Mock
+	CarParkingInfo carParkingInfo;
 
 	@Test
 	void contextLoads() {
+		Assertions.assertNotNull(carParkingInfo);
 	}
 
 }
