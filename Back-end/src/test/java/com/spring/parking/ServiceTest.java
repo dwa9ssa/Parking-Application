9caed4dd1e3e0b93160cc7ab301f281541eda40a
Badr Ouaddah <Badr.Ouaddah.07@gmail.com>
2023-12-13 21:50:15 +0100

    Create ServiceTest with testCarService test method

---
+++ Back-end/src/test/java/com/spring/parking/ServiceTest.java
@@ -0,0 +1,54 @@
+package com.spring.parking;
+
+import com.spring.parking.dao.CarParkingInfoDao;
+import com.spring.parking.dto.CarParkingInfoDto;
+import com.spring.parking.entity.CarParkingInfo;
+import com.spring.parking.mapper.CarParkingInfoMapper;
+import com.spring.parking.mapper.ParkingLotMapper;
+import com.spring.parking.service.ParkingLotService;
+import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class ServiceTest {
+
+
+    @InjectMocks
+    ParkingLotService parkingLotService;
+
+    @InjectMocks
+    CarParkingInfoMapper carParkingInfoMapper;
+
+
+    @Mock
+    private CarParkingInfo carParkingInfo;
+
+    @Mock
+    private CarParkingInfoDao carParkingInfoDao;
+
+
+    @Test
+    public void testCarService(){
+        CarParkingInfo carParkingInfo = Mockito.mock(CarParkingInfo.class);
+
+        CarParkingInfoDto carParkingInfoDto = CarParkingInfoDto.builder()
+                .vehicleRegistration(1L)
+                .brand("TestBrand")
+                .color("TestColor")
+                .type("TestType")
+                .totalPrice(10)
+                .build();
+
+        when(carParkingInfoMapper.toCarParkingInfoEntity(carParkingInfoDto)).thenReturn(carParkingInfo);
+        when(carParkingInfoDao.getReferenceById(carParkingInfo.getVehicleRegistration())).thenReturn(carParkingInfo);
+        parkingLotService.parkingCar(1L, carParkingInfoDto);
+    }
+
+
+}
